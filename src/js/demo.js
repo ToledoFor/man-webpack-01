a81bf95017b7a71273fbfb5f0724fa91e3591391
Dmitriy Olhovsky <dmitriy.olhovsky@beetroot.se>
2023-02-17 18:07:08 +0200

    Code refactor + JS intro

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,36 @@
-const demo = () => 'Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+const demo = () => '1 Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+
+const demo1 = function() {
+    return '2 Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+}
 
 // eslint-disable-next-line no-console
 console.log(demo());
+console.log(demo1());
+
+let var1 = 10;
+let var2 = 20;
+
+let var3;
+var3 = var1 + var2;
+
+console.log(var3);
+
+// const variable1 = 10;
+
+// let variable2 = "test";
+
+// var variable3 = 123;
+
+// variable2 = 6;
+
+// let testObject = {
+//     key1: "test",
+//     key2: 12,
+// }
+
+// testObject.key1 = "test1";
+
+// function testFunction() {
+//     const variable1 = 10;
+// }
