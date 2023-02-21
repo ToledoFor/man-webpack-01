50f2960d59e14d3871a5e6d7d968db57fabb2778
Dmitriy Olhovsky <dmitriy.olhovsky@beetroot.se>
2023-02-21 15:59:09 +0200

    Add slider (swiper)

--- src/js/demo.js
+++ src/js/demo.js
@@ -4,6 +4,14 @@ const demo1 = function() {
     return '2 Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
 }
 
+const sliderParameters = {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+};
+
+const swiper = new Swiper('#features-slider', sliderParameters);
+
 // eslint-disable-next-line no-console
 console.log(demo());
 console.log(demo1());
